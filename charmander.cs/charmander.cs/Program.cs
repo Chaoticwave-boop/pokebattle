a3b1f87cdb33fc5680424897053e5a4a39c70da5
Chaoticwave-boop <bren.stoute@hotmail.com>
2023-05-10 12:19:47 +0200

    class added + for loop + while loop and break and input for name

---
+++ charmander.cs/charmander.cs/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using poke;
+
+class main
+{
+    static void Main(string[] args)
+    {
+
+        bool gamestart = true;
+
+        while (gamestart)
+        {
+
+            pokemon bulbasaur = new pokemon("bulbasaur", "grass", "fire", "");
+
+            Console.WriteLine("enter your charmanders name");
+            string Pokename = Console.ReadLine();
+
+            pokemon charmander = new pokemon("charmander", "grass", "fire", Pokename);
+
+            Console.WriteLine("pokename:" + charmander.pokename);
+            Console.WriteLine(charmander.name);
+            Console.WriteLine(charmander.strenght);
+            Console.WriteLine(charmander.weakness);
+
+            Console.WriteLine("---------");
+
+            Console.WriteLine(bulbasaur.name);
+            Console.WriteLine(bulbasaur.strenght);
+            Console.WriteLine(bulbasaur.weakness);
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine((i + 1) + "--" + "CHARMANDER!");
+            };
+
+
+            if (Pokename == "exit") {
+                break;
+            }
+        }
+
+       
+
+    }
+}
---
+++ charmander.cs/charmander.cs/pokemon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poke
+{
+    internal class pokemon
+    {
+        public string name;
+        public string strenght = "fire";
+        public string weakness = "water";
+        public string pokename = "charmander";
+
+        public pokemon() { }
+
+        public pokemon(string name, string strenght, string weakness, string pokename)
+        {
+            this.name = name;
+            this.strenght = strenght;
+            this.weakness = weakness;
+            this.pokename = pokename;
+        }
+    }
+}
